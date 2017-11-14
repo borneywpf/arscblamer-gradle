b16a17ab75d107c8b60940927a165752b0c4702f
borney <borneywpf@gmail.com>
2017-11-14 10:52:25 +0800

    create arscblamer-gradle

---
+++ src/main/java/com/think/arsc/ArscDumper.java
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.think.arsc;
+
+import com.google.common.base.Preconditions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.annotation.Nullable;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+/**
+ * Pulls useful information from an APK's resources.arsc file. This can be used to see the different
+ * resource configurations in the APK, their size, and the entries in those configurations.
+ * <p>
+ * <p>This can also be used to get a list of the different entry names, or to get a list of resource
+ * entries for which no default value exists (baseless keys).
+ * <p>
+ * <p>Example usage to save all resource configurations to a CSV file:
+ * <p>
+ * <pre>ArscDumper.jar --apk=/apk_dir/my.apk --output=/csv_dir/my.csv --type=configs</pre>
+ *
+ * <p>This CSV could then be sorted by "Null Entries" in descending order to spot resource
+ * configurations that could potentially be removed for large byte savings.
+ */
+public class ArscDumper {
+
+  private static final String RESOURCES_ARSC = "resources.arsc";
+  private final ResourceTableChunk chunk;
+
+  /**
+   * Creates a new {@link ArscDumper}.
+   */
+  public ArscDumper(ResourceTableChunk chunk) {
+    this.chunk = chunk;
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(Arrays.toString(args));
+    if (args.length >= 1) {
+      File apk = new File(args[0]);
+      if (apk.exists()) {
+        ResourceTableChunk tableChunk = providesResourceTableChunk(apk);
+        ArscDumper dumper = new ArscDumper(tableChunk);
+        dumper.dumpRunTableChunk(tableChunk);
+        dumper.dumpWriteTableChunk(tableChunk);
+      }
+    }
+  }
+
+  /**
+   * Returns a CSV row (as a list of strings) describing a particular resource configuration. If
+   * showKeys is true, the "Keys" column will be populated with the keys of the resource entries in
+   * that configuration. Otherwise, the "Keys" column will be blank.
+   */
+  private void dumpWriteTableChunk(ResourceTableChunk chunk) {
+    WritableWorkbook book = null;
+    try {
+
+      book = Workbook.createWorkbook(new File("/home/borney/data/tmp/arsc.xls"));
+      StringPoolChunk chunkStringPool = chunk.getStringPool();
+      Collection<PackageChunk> chunkPackages = chunk.getPackages();
+      for (PackageChunk chunkPackage : chunkPackages) {
+
+        StringPoolChunk typeStringPool = chunkPackage.getTypeStringPool();
+        for (int index = 0, count = typeStringPool.getStringCount(); index < count; index++) {
+          String type = typeStringPool.getString(index);
+          WritableSheet sheet = book.createSheet(type, index);
+          Collection<TypeChunk> typeChunks = chunkPackage.getTypeChunks(type);
+
+          writeHead(sheet, typeChunks);
+
+          writeID(sheet, typeChunks, chunkPackage);
+
+          writeName(sheet, typeChunks);
+
+          writeEntryValue(chunkStringPool, sheet, typeChunks);
+        }
+      }
+
+      book.write();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (RowsExceededException e) {
+      e.printStackTrace();
+    } catch (WriteException e) {
+      e.printStackTrace();
+    } finally {
+      if (book != null) {
+        try {
+          book.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        } catch (WriteException e) {
+          e.printStackTrace();
+        }
+      }
+
+
+    }
+  }
+
+  private void writeID(WritableSheet sheet, Collection<TypeChunk> typeChunks, PackageChunk chunkPackage) throws WriteException {
+    int cell = 0;
+    for (TypeChunk typeChunk : typeChunks) {
+      Collection<TypeChunk.Entry> values = typeChunk.getEntries().values();
+      int row = 1;
+      for (int i = 0; i < values.size(); i++) {
+        String id = "0x" + Integer.toHexString(chunkPackage.getId()) + String.format("%02X", typeChunk.getId()) + String.format("%04X", i);
+        Label label = new Label(cell, row, id);
+        sheet.addCell(label);
+        row++;
+      }
+    }
+  }
+
+  private void writeEntryValue(StringPoolChunk chunkStringPool, WritableSheet sheet, Collection<TypeChunk> typeChunks) throws WriteException {
+    int cell = 2;
+    for (TypeChunk typeChunk : typeChunks) {
+      Set<Entry<Integer, TypeChunk.Entry>> entries = typeChunk.getEntries().entrySet();
+      for (Entry<Integer, TypeChunk.Entry> entry : entries) {
+        TypeChunk.Entry value = entry.getValue();
+        int row = getRowByName(typeChunks, value.key());
+        Label label = null;
+        if (value.isComplex()) {
+          Map<Integer, ResourceValue> resourceValueMap = value.getValues();
+          Collection<ResourceValue> resourceValues = resourceValueMap.values();
+          StringBuilder sb = new StringBuilder();
+          for (ResourceValue resourceValue : resourceValues) {
+            sb.append(getResourcesValue(resourceValue, chunkStringPool)).append(',');
+          }
+          String s = sb.toString();
+          if (s.length() > 0) {
+            label = new Label(cell, row, s.substring(0, s.length() - 1));
+          }
+        } else {
+          ResourceValue resourceValue = value.getValue();
+          label = new Label(cell, row, getResourcesValue(resourceValue, chunkStringPool));
+        }
+        if (label != null) {
+          sheet.addCell(label);
+        } else {
+          sheet.addCell(new Label(cell, row, ""));
+        }
+      }
+      cell++;
+    }
+  }
+
+  private String getResourcesValue(ResourceValue resourceValue, StringPoolChunk chunkStringPool) {
+    int data = resourceValue.getData();
+    switch (resourceValue.getType()) {
+      case STRING:
+        return chunkStringPool.getString(data);
+      case DIMENSION:
+        return String.format("dimension(%d)", data);
+      case INT_COLOR_RGB8:
+        return String.format("rgb8(%s)", Integer.toHexString(data));
+      case INT_COLOR_ARGB4:
+        return String.format("argb4(%s)", Integer.toHexString(data));
+      case INT_COLOR_ARGB8:
+        return String.format("argb8(%s)", Integer.toHexString(data));
+      case INT_COLOR_RGB4:
+        return String.format("argb4(%s)", Integer.toHexString(data));
+      case INT_HEX:
+        return Integer.toHexString(data);
+      case INT_DEC:
+        return Integer.toString(data);
+      case ATTRIBUTE:
+        return String.valueOf(data);
+      case INT_BOOLEAN:
+        return data == 1 ? "true" : "false";
+      case REFERENCE:
+        return "@ref/0x" + Integer.toHexString(data);
+      case FLOAT:
+        return Float.toString(data);
+      case FRACTION:
+      case DYNAMIC_REFERENCE:
+      case NULL:
+      default:
+        break;
+    }
+    return "";
+  }
+
+  private int getRowByName(Collection<TypeChunk> typeChunks, String key) {
+    for (TypeChunk typeChunk : typeChunks) {
+      Set<Entry<Integer, TypeChunk.Entry>> entries = typeChunk.getEntries().entrySet();
+      int row = 1;
+      for (Entry<Integer, TypeChunk.Entry> entry : entries) {
+        TypeChunk.Entry value = entry.getValue();
+        if (value.key().equals(key)) {
+          return row;
+        }
+        row++;
+      }
+    }
+    return -1;
+  }
+
+  private void writeName(WritableSheet sheet, Collection<TypeChunk> typeChunks) throws WriteException {
+    int cell = 1;
+    for (TypeChunk typeChunk : typeChunks) {
+      Set<Entry<Integer, TypeChunk.Entry>> entries = typeChunk.getEntries().entrySet();
+      int row = 1;
+      for (Entry<Integer, TypeChunk.Entry> entry : entries) {
+        TypeChunk.Entry value = entry.getValue();
+        Label label = new Label(cell, row, value.key());
+        sheet.addCell(label);
+        row++;
+      }
+    }
+  }
+
+  private void writeHead(WritableSheet sheet, Collection<TypeChunk> typeChunks) throws WriteException {
+    int cell = 0;
+    sheet.addCell(new Label(cell++, 0, "ID"));
+    sheet.addCell(new Label(cell++, 0, "Name"));
+    for (TypeChunk typeChunk : typeChunks) {
+      ResourceConfiguration configuration = typeChunk.getConfiguration();
+      Label label = new Label(cell, 0, configuration.toString());
+      sheet.addCell(label);
+      cell++;
+    }
+  }
+
+  private void dumpRunTableChunk(ResourceTableChunk chunk) {
+    System.out.println("  |--StringPoolChunk");
+    StringPoolChunk stringPool = chunk.getStringPool();
+    for (int index = 0, count = stringPool.getStringCount(); index < count; index++) {
+      System.out.println("    index[" + index + "]=" + stringPool.getString(index));
+    }
+    for (int index = 0, count = stringPool.getStyleCount(); index < count; index++) {
+      System.out.println("    index[" + index + "]=" + stringPool.getStyle(index));
+    }
+    Collection<PackageChunk> chunkPackages = chunk.getPackages();
+    for (PackageChunk chunkPackage : chunkPackages) {
+      System.out.println("  |--PackageChunk");
+      int id = chunkPackage.getId();
+      System.out.println("    id=" + id);
+      String packageName = chunkPackage.getPackageName();
+      System.out.println("    packageName=" + packageName);
+
+      StringPoolChunk typeStringPool = chunkPackage.getTypeStringPool();
+      System.out.println("    |--typeStringPool");
+      for (int index = 0, count = typeStringPool.getStringCount(); index < count; index++) {
+        System.out.println("      index[" + index + "]=" + typeStringPool.getString(index));
+      }
+      for (int index = 0, count = typeStringPool.getStyleCount(); index < count; index++) {
+        System.out.println("      index[" + index + "]=" + typeStringPool.getStyle(index));
+      }
+
+      StringPoolChunk keyStringPool = chunkPackage.getKeyStringPool();
+      System.out.println("    |--keyStringPool");
+      for (int index = 0, count = keyStringPool.getStringCount(); index < count; index++) {
+        System.out.println("      index[" + index + "]=" + keyStringPool.getString(index));
+      }
+      for (int index = 0, count = keyStringPool.getStyleCount(); index < count; index++) {
+        System.out.println("      index[" + index + "]=" + keyStringPool.getStyle(index));
+      }
+
+      Collection<TypeSpecChunk> specChunks = chunkPackage.getTypeSpecChunks();
+      System.out.println("    |--specChunks");
+      for (TypeSpecChunk specChunk : specChunks) {
+        int specChunkId = specChunk.getId();
+        int resourceCount = specChunk.getResourceCount();
+        System.out.println("      specChunkId:" + specChunkId + " resourceCount:" + resourceCount);
+      }
+
+      System.out.println("    |--typeChunks");
+      Collection<TypeChunk> typeChunks = chunkPackage.getTypeChunks();
+      for (TypeChunk typeChunk : typeChunks) {
+        int typeChunkId = typeChunk.getId();
+        ResourceConfiguration configuration = typeChunk.getConfiguration();
+        System.out.println("      typeChunkId:" + typeChunkId);
+        System.out.println("      configuration:" + configuration);
+        Set<Entry<Integer, TypeChunk.Entry>> entries = typeChunk.getEntries().entrySet();
+        for (Entry<Integer, TypeChunk.Entry> entry : entries) {
+          TypeChunk.Entry value = entry.getValue();
+          String key = value.key();
+          int flags = value.getFlags();
+          String valueStr = "";
+          if (value.isComplex()) {
+            Map<Integer, ResourceValue> resourceValueMap = value.getValues();
+            Collection<ResourceValue> resourceValues = resourceValueMap.values();
+            StringBuilder sb = new StringBuilder();
+            for (ResourceValue resourceValue : resourceValues) {
+              sb.append(getResourcesValue(resourceValue, stringPool)).append(',');
+            }
+            String s = sb.toString();
+            if (s.length() > 0) {
+              valueStr = s.substring(0, s.length() - 1);
+            }
+          } else {
+            ResourceValue resourceValue = value.getValue();
+            valueStr = getResourcesValue(resourceValue, stringPool);
+          }
+          System.out.println("        key:" + key + " flags:" + flags + " valueValue:" + valueStr);
+        }
+      }
+    }
+  }
+
+  /**
+   * Provides the resources.arsc resource table in the {@code apk}.
+   */
+  private static ResourceTableChunk providesResourceTableChunk(@Nullable File apk) throws IOException {
+    Preconditions.checkNotNull(apk, "APK is required. Did you forget --apk=/my/app.apk?");
+    byte[] resourceBytes = ApkUtils.getFile(apk, RESOURCES_ARSC);
+    if (resourceBytes == null) {
+      throw new IOException(String.format("Unable to find %s in APK.", RESOURCES_ARSC));
+    }
+    List<Chunk> chunks = new ResourceFile(resourceBytes).getChunks();
+    Preconditions.checkState(chunks.size() == 1,
+            "%s should only have one root chunk.", RESOURCES_ARSC);
+    Chunk resourceTable = chunks.get(0);
+    Preconditions.checkState(resourceTable instanceof ResourceTableChunk,
+            "%s root chunk must be a ResourceTableChunk.", RESOURCES_ARSC);
+    return (ResourceTableChunk) resourceTable;
+  }
+}
---
+++ src/main/java/com/think/arsc/XmlAttribute.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.think.arsc;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/** Represents an XML attribute and value. */
+public class XmlAttribute implements SerializableResource {
+
+  /** The serialized size in bytes of an {@link XmlAttribute}. */
+  public static final int SIZE = 12 + ResourceValue.SIZE;
+
+  /** A string reference to the namespace URI, or -1 if not present. */
+  private int namespaceIndex;
+
+  /** A string reference to the attribute name. */
+  private int nameIndex;
+
+  /** A string reference to a string containing the character value.  */
+  private int rawValueIndex;
+
+  /** A {@link ResourceValue} instance containing the parsed value. */
+  private ResourceValue typedValue;
+
+  /** The parent of this XML attribute; used for dereferencing the namespace and name. */
+  private XmlNodeChunk parent;
+
+  public XmlAttribute(int namespaceIndex, int nameIndex, int rawValueIndex, ResourceValue typedValue, XmlNodeChunk parent) {
+    this.namespaceIndex = namespaceIndex;
+    this.nameIndex = nameIndex;
+    this.rawValueIndex = rawValueIndex;
+    this.typedValue = typedValue;
+    this.parent = parent;
+  }
+
+  /** The namespace URI, or the empty string if not present. */
+  public final String namespace() {
+    return getString(namespaceIndex);
+  }
+
+  /** The attribute name, or the empty string if not present. */
+  public final String name() {
+    return getString(nameIndex);
+  }
+
+  /** The raw character value. */
+  public final String rawValue() {
+    return getString(rawValueIndex);
+  }
+
+  /**
+   * Creates a new {@link XmlAttribute} based on the bytes at the current {@code buffer} position.
+   *
+   * @param buffer A buffer whose position is at the start of a {@link XmlAttribute}.
+   * @param parent The parent chunk that contains this attribute; used for string lookups.
+   */
+  public static XmlAttribute create(ByteBuffer buffer, XmlNodeChunk parent) {
+    int namespace = buffer.getInt();
+    int name = buffer.getInt();
+    int rawValue = buffer.getInt();
+    ResourceValue typedValue = ResourceValue.create(buffer);
+    return new XmlAttribute(namespace, name, rawValue, typedValue, parent);
+  }
+
+  private String getString(int index) {
+    return parent.getString(index);
+  }
+
+  @Override
+  public byte[] toByteArray() {
+    return toByteArray(false);
+  }
+
+  @Override
+  public byte[] toByteArray(boolean shrink) {
+    ByteBuffer buffer = ByteBuffer.allocate(SIZE).order(ByteOrder.LITTLE_ENDIAN);
+    buffer.putInt(namespaceIndex);
+    buffer.putInt(nameIndex);
+    buffer.putInt(rawValueIndex);
+    buffer.put(typedValue.toByteArray(shrink));
+    return buffer.array();
+  }
+
+  /**
+   * Returns a brief description of this XML attribute. The representation of this information is
+   * subject to change, but below is a typical example:
+   *
+   * <pre>"XmlAttribute{namespace=foo, name=bar, value=1234}"</pre>
+   */
+  @Override
+  public String toString() {
+    return String.format("XmlAttribute{namespace=%s, name=%s, value=%s}",
+        namespace(), name(), rawValue());
+  }
+}
